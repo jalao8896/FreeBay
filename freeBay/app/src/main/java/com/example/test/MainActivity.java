b24b412b98c3b932e5ffb57e073f130b89480af8
Andy Chen <andychen159@gmail.com>
2019-11-05 16:21:32 -0800

    Added a working camera feature

--- freeBay/app/src/main/java/com/example/test/MainActivity.java
+++ freeBay/app/src/main/java/com/example/test/MainActivity.java
@@ -2,51 +2,91 @@ package com.example.test;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import dmax.dialog.SpotsDialog;
 
+import android.Manifest;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraView;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
-    CameraView cameraView;
-    Button btnDetect;
-    AlertDialog waitingDialog;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.start();
-
-    }
-
-    @Override
-    protected void onPause (){
-        super.onPause();
-        cameraView.stop();
-
-    }
+    ImageButton btnTakePic;
+    ImageView imageView;
+    String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraView = (CameraView)findViewById(R.id.camera_view);
-        btnDetect = (Button)findViewById(R.id.btn_detect);
+        btnTakePic = findViewById(R.id.camera_button);
 
-        waitingDialog = new SpotsDialog.Builder().setMessage("Please waiting...")
-                .setCancelable(false).build();
+        if(Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
 
-        cameraView.addCameraKitListener(){
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                dispatchPictureTakerAction();
+            }
+        });
 
-            public void onImage(CameraKitImage cameraKitImage){
-                waitingDialog.show();
-                Bitmap bitmap = cameraKitImage.getBitmap();
-                bitmap = Bitmap.createScaledBitmap(bitmap,cameraView, cameraView.getWidth(),cameraView.getHeight(), false);
+    }
+
+    private void dispatchPictureTakerAction() {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePic.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if(photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "admin", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = null;
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            return image;
+//        }
+        image = new File(storageDir, name);
+        return image;
+    }
+
+
+    protected void OnActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                imageView.setImageBitmap(bitmap);
             }
         }
     }

3d2edb6d015ad7027bb5565b994f77c5b6a0deba
Kyle Jacobs <kycjacob@ucsc.edu>
2019-11-13 16:23:35 -0800

    Added Multiple classes, XML files, hard data for use with cardview

--- freeBay/app/src/main/java/com/example/test/MainActivity.java
+++ freeBay/app/src/main/java/com/example/test/MainActivity.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.test;
 
 import androidx.annotation.NonNull;
@@ -174,4 +175,187 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+=======
+package com.example.test;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.example.test.MainActivity2;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button takePic;
+    String pathToFile;
+    Button chooseImg;
+    Button uploadImg;
+    ImageView imgView;
+    ProgressDialog pd;
+    static final int PICK_IMAGE_REQUEST = 1;
+    Uri filePath;
+
+
+
+    //AD ACTIVITY STUFF
+    //------------------------------------------------------------
+    private Button adButton;
+
+    public void openMainActivity2(){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+    //------------------------------------------------------------
+
+
+
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmssSSS_z");
+    String currentDateandTime = formatter.format(new Date());
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://freebay-cbb54.appspot.com");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //SHOULD OPEN AD LISTING PAGE FROM MAIN ACTIVITY
+        //------------------------------------------------------------
+        adButton = (Button)findViewById(R.id.view_ad_button);
+
+        adButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity2();
+            }
+        });
+        //------------------------------------------------------------
+
+       takePic = findViewById(R.id.cameraButton);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+
+        takePic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                dispatchPictureTakerAction();
+            }
+        });
+
+        chooseImg = (Button) findViewById(R.id.chooseImg);
+        uploadImg = (Button) findViewById(R.id.uploadImg);
+        imgView = (ImageView) findViewById(R.id.itemImage);
+
+        pd = new ProgressDialog(this);
+        pd.setMessage("Uploading....");
+
+        chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+        uploadImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (filePath != null) {
+                    pd.show();
+
+                    StorageReference childRef = storageRef.child("IMG_" + currentDateandTime + ".jpg");
+
+                    //uploading the image
+                    UploadTask uploadTask = childRef.putFile(filePath);
+
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            pd.dismiss();
+                            Toast.makeText(MainActivity.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(MainActivity.this, "Upload Failed -> " + e, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Select an image", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void dispatchPictureTakerAction() {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePic.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                filePath = FileProvider.getUriForFile(MainActivity.this, "admin", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, filePath);
+                startActivityForResult(takePic, 1);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = null;
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            return image;
+//        }
+        image = new File(storageDir, name);
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            //getting image from gallery
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+            //Setting image to ImageView
+            imgView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+>>>>>>> 96a330c... Added: Advertisement Class, RecyclerView Adapter Class, Cardview Items, Advertisement Activity triggered from edited main activity. Included: hard data for use in cardview.
 }
